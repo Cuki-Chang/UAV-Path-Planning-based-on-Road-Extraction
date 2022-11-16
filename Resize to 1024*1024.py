db08e81c163e2b1261663e30c709752fde75c88d
Chang Liu <changliu@hit.bme.hu>
2022-11-16 11:15:17 +0100

    Update Resize to 1024*1024.py

--- Resize to 1024*1024.py
+++ Resize to 1024*1024.py
@@ -1,10 +1,10 @@
 import cv2
 from PIL import Image as ImagePIL
 from PIL import Image
-im = ImagePIL.open('IMG_170410_132553_0155_RGB.JPG')
+im = ImagePIL.open('I.JPG')
 print(im)
 print(type(im))
-im = cv2.imread('IMG_170410_132553_0155_RGB.JPG')
+im = cv2.imread('I.JPG')
 
 #image = Image.fromarray(cv2.cvtColor(im,cv2.COLOR_BGR2RGB))  #格式转换，bgr转rgb
 #image.save('qq1.jpg',quality=95,dpi=(1024.0,1024.0))    #调整图像的分辨率为300,dpi可以更改
@@ -14,6 +14,6 @@ pic = im[0:1024,0:1024,:]
 
 image = Image.fromarray(cv2.cvtColor(pic,cv2.COLOR_BGR2RGB))
 
-#image.save('MIMG_1205.JPG')
+#image.save('M.JPG')
 
 image.save('out1024.JPG',quality=95,dpi=(72.0,72.0))    #调整图像的分辨率
